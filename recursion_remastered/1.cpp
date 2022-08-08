44e83fae49579806ee334fe5e39336cb7449abe4
singhxayush <fanduayush2000@gmail.com>
2022-08-08 14:59:39 +0530

    x

---
+++ recursion_remastered/1.cpp
@@ -0,0 +1,26 @@
+// 
+
+#include<iostream>
+using namespace std;
+void one_to_n(int n)
+{
+    if(n)
+    {
+        one_to_n(n-1);
+        cout<<n<<"\n";
+    }
+}
+
+void n_to_one(int n)
+{
+    if(n)
+    {
+        cout<<n<<"\n";
+        n_to_one(n-1);
+    }
+}
+int main()
+{
+    // one_to_n(5);
+    n_to_one(5);
+}
