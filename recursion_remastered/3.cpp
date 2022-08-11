7602941d0a8c60826d7d4761fe2e15e153f72832
singhxayush <fanduayush2000@gmail.com>
2022-08-11 23:18:35 +0530

    x

--- recursion_remastered/3.cpp
+++ recursion_remastered/3.cpp
@@ -1,9 +1,27 @@
-// 
 #include<iostream>
 using namespace std;
-#define pr(x) cout<<(x)<<endl;
-#define  dbg(x) cout<<'d'<<'b'<<'g'<<'-'<<'>'<<x<<endl;
+
+typedef long long  ll;
+
+static int move_counts = 0;
+
+void toh(int N, char a, char b, char c, int n) // c -> destination, >
+{   
+    move_counts++;
+    if(N==1)
+    {
+        cout<<a<<c<<" ";
+        return;
+    }
+    toh(N-1, a, c, b, n);
+    cout<<a<<c<<" ";
+    toh(N-1, b, a, c, n);
+}
 int main()
 {
-    
+    int N; cout<<"Enter Number of Discs :"; cin>>N;
+    int n; cin>>n;
+
+    toh(N, 'a', 'b', 'c', n);
+    cout<<"\nTotal number of moves :"<<move_counts<<endl;
 }
