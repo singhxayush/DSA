20480187a29c36abaf3c7f2c445b9118d8a704f3
singhxayush <fanduayush2000@gmail.com>
2022-10-06 20:38:24 +0530

    x

--- recursion_remastered/1.cpp
+++ recursion_remastered/1.cpp
@@ -1,9 +1,20 @@
-//
-
+#include<bits/stdc++.h>
 #include"recursion.h"
-#define  dbg(x) cout<<'d'<<'b'<<'g'<<'-'<<'>'<<x<<endl;
 
 int main()
 {
-    
+    // oneton(4);
+    // ntoone(6);
+
+    int n = 7;
+    int a[n] = {5, 19, -1, 0, 5, 3, 1};
+    sortarray(a, n); for(int i=0; i<n; i++) cout<<a[i]<<" ";
+
+    // int n = 1;
+    // int a[n] = {2};
+    // sortarray(a, n); for(int i=0; i<n; i++) cout<<a[i]<<" ";
+
+    // vector<int> v{5, 19, -1, 0, 5, 3, 1};
+    // sortarray(v);
+    // for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
 }
--- recursion_remastered/recursion.h
+++ recursion_remastered/recursion.h
@@ -1,12 +1,62 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+#define  dbg(x) cout<<'d'<<'b'<<'g'<<'-'<<'>'<<x<<endl;
+
 
 void oneton(int n)
 {
-
+    if(n==0) return;
+    oneton(n-1);
+    cout<<n<<" ";
 }
-
 void ntoone(int n)
 {
-    
+    if(n==0) return;
+    cout<<n<<" ";
+    ntoone(n-1);
+}
+
+// // sorting using recursive insertion sort
+
+// void insert_r(vector<int> &v, int key) {
+//     if(v.size()==0 || v.back()<=key){ 
+//         v.push_back(key);
+//         return; 
+//     }
+//     int val = v.back();
+//     v.pop_back();
+//     insert_r(v, key);
+//     v.push_back(val);
+//     return;
+// }
+// void sortarray(vector<int> &v) {
+//     if(v.size()==1) return;
+//     int key = v.back();
+//     v.pop_back();
+//     sortarray(v);
+//     insert_r(v, key);
+// }
+
+int top;
+void insert_r(int a[], int key, int top) 
+{
+    dbg(a[top])
+
+    if(top==0 || a[top]<=key) {
+        a[top] = key;
+        return;
+    }
+    int val = a[top--];
+    insert_r(a, key, top);
+    a[++top] = val;
+    return;
+}
+void sortarray(int a[], int n) {
+    if(n==0) return;
+    sortarray(a, n-1);
+    top = n-1;
+    int key = a[top];
+    insert_r(a, key, top);
 }
