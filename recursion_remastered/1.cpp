b051299022398f9792e6a26840d7600c7586a404
singhxayush <fanduayush2000@gmail.com>
2022-10-06 13:23:19 +0530

    x

--- recursion_remastered/1.cpp
+++ recursion_remastered/1.cpp
@@ -1,30 +1,9 @@
-// 
-#include<iostream>
-using namespace std;
+//
+
+#include"recursion.h"
 #define  dbg(x) cout<<'d'<<'b'<<'g'<<'-'<<'>'<<x<<endl;
 
-void one_to_n(int n)
-{
-    if(n)
-    {
-        one_to_n(n-1);
-        cout<<n<<"\n";
-    }
-    return;
-    cout<<0<<"\n";
-}
-void n_to_one(int n)
-{
-    if(n)
-    {
-        cout<<n<<"\n";
-        n_to_one(n-1);
-    }
-    return;
-    cout<<0<<"\n";
-}
 int main()
 {
-    // one_to_n(5);
-    n_to_one(5);
+    
 }
---
+++ recursion_remastered/recursion.h
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void oneton(int n)
+{
+
+}
+
+void ntoone(int n)
+{
+    
+}
