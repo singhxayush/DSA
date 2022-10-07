bc804ef1e233f3b6beab445dc4358c409fcad94f
singhxayush <fanduayush2000@gmail.com>
2022-10-07 17:02:53 +0530

    x

--- recursion_remastered/1.cpp
+++ recursion_remastered/1.cpp
@@ -3,9 +3,11 @@
 
 int main()
 {
+    //print 1 to N and reverse
     // oneton(4);
     // ntoone(6);
 
+    //sort array
     // int n = 7;
     // int a[n] = {5, 19, -1, 0, 5, 3, 1};
     // sortarray(a, n); for(int i=0; i<n; i++) cout<<a[i]<<" ";
@@ -14,7 +16,27 @@ int main()
     // int a[n] = {2};
     // sortarray(a, n); for(int i=0; i<n; i++) cout<<a[i]<<" ";
 
+    //sort vector
     // vector<int> v{5, 19, -1, 0, 5, 3, 1};
     // sortarray(v);
     // for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+
+    //sort stack
+    stack<int> s;
+    s.push(1);
+    s.push(4);
+    s.push(0);
+    s.push(-1);
+    s.push(2);
+    s.push(0);
+
+    // sort_stack(s);
+    // int pos = s.size()/2+1;
+    // delete_midelm_stack(s, pos);
+    // sort_stack_rev(s);
+    reverse_stack(s);
+    while(!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
 }
--- recursion_remastered/recursion.h
+++ recursion_remastered/recursion.h
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<stack>
 using namespace std;
 #define  dbg(x) cout<<'d'<<'b'<<'g'<<'-'<<'>'<<x<<endl;
 
@@ -18,47 +19,130 @@ void ntoone(int n)
     ntoone(n-1);
 }
 
-// sorting using recursive insertion sort
+//// sorting using recursive insertion sort ////
 
-// void insert_r(vector<int> &v, int key) {
-//     if(v.size()==0 || v.back()<=key){ 
-//         v.push_back(key);
-//         return; 
-//     }
-//     int val = v.back();
-//     v.pop_back();
-//     insert_r(v, key);
-//     v.push_back(val);
-//     return;
-// }
-// void sortarray(vector<int> &v) {
-//     if(v.size()==1) return;
-//     int key = v.back();
-//     v.pop_back();
-//     sortarray(v);
-//     insert_r(v, key);
-// }
-
-
-int top = 0;
-void insert_r(int a[], int key) 
-{
-    // cout<<top<<" "<<a[top]<<" "<<key<<endl;
-
-    if(top==0 || a[top-1]<=key) {
-        a[top] = key;
-        return;
+void insert_r(vector<int> &v, int key) {
+    if(v.size()==0 || v.back()<=key){ 
+        v.push_back(key);
+        return; 
     }
-    int val = a[top--];
-    insert_r(a, key);
-    a[++top] = val;
+    int val = v.back();
+    v.pop_back();
+    insert_r(v, key);
+    v.push_back(val);
     return;
 }
-void sortarray(int a[], int n) {
-    if(n==0) return;
-    sortarray(a, n-1);
-    int top;
-    top = n-1;
-    int key = a[top];
-    insert_r(a, key);
+void sortarray(vector<int> &v) {
+    if(v.size()==1) return;
+    int key = v.back();
+    v.pop_back();
+    sortarray(v);
+    insert_r(v, key);
+}
+
+// int top = 0;
+// void insert_r(int a[], int key) 
+// {
+//     // cout<<top<<" "<<a[top]<<" "<<key<<endl;
+
+//     if(top==0 || a[top-1]<=key) {
+//         a[top] = key;
+//         return;
+//     }
+//     int val = a[top--];
+//     insert_r(a, key);
+//     a[++top] = val;
+//     return;
+// }
+// void sortarray(int a[], int n) {
+//     if(n==0) return;
+//     sortarray(a, n-1);
+//     int top;
+//     top = n-1;
+//     int key = a[top];
+//     insert_r(a, key);
+// }
+
+void insert_r(stack<int> &s, int key)
+{
+    if(s.empty() || s.top()<=key)
+    {
+        s.push(key);
+        return;
+    }
+    int val = s.top();
+    s.pop();
+    insert_r(s, key);
+    s.push(val);
+    return;
+}
+void sort_stack(stack<int> &s)
+{
+    if(s.size()==1) return;
+    int key = s.top();
+    s.pop();
+    sort_stack(s);
+    insert_r(s, key);
+}
+
+void delete_midelm_stack(stack<int> &s, int pos)
+{
+    if(s.size()==pos)
+    {
+        s.pop();
+        return;
+    }
+    
+    int key = s.top();
+    s.pop();
+    delete_midelm_stack(s, pos);
+    s.push(key);
+}
+
+void sort_stack_rev(stack<int> &s)
+{
+    if(s.size()==1) return;
+    int key = s.top();
+    s.pop();
+    sort_stack_rev(s);  
+
+    //insert section
+    stack<int> temp;
+    while(!s.empty() && key<s.top())
+    {
+        temp.push(s.top());
+        s.pop();
+    }
+    s.push(key);
+    while(!temp.empty())
+    {
+        s.push(temp.top());
+        temp.pop();
+    }
+    return;
+}
+
+void insert(stack<int> &s, int key)
+{
+    if(s.empty()){
+        s.push(key);
+        return;
+    }
+    
+    int x = s.top();
+    s.pop();
+
+    insert(s, key);
+
+    s.push(x);
+}
+void reverse_stack(stack<int> &s)
+{
+    if(s.size()==1) return;
+
+    int key = s.top();
+    s.pop();
+    reverse_stack(s);
+
+    insert(s, key);
 }
