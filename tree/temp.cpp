daa05fc5d8b78c114464791980f2e392150062d9
singhxayush <fanduayush2000@gmail.com>
2022-10-25 03:15:09 +0530

    x

--- tree/temp.cpp
+++ tree/temp.cpp
@@ -1,6 +1,6 @@
 /////// Tree Traversal ///////
 
-#include"tree.h"
+#include"treex.h"
 
 int main()
 {
@@ -17,8 +17,27 @@ int main()
     root1->l->l->r = new node(9);
     root1->l->l->r->l = new node(10);
     
-    // preorder(root1);
-    postorder(root1);
+
+    preorder(root1); cout<<endl;
+    // inorder(root1); cout<<endl;
+    // postorder(root1); cout<<endl;
+
+    // cout<<"Height of tree : "<<height(root1)<<"\n";
+
+    // int d;
+    // cout<<"Enter distance :"; cin>>d;
+    // cout<<"Nodes at distance "<<d<<" is "; node_at_distance_(d-1, root1); cout<<endl;
+
+    // levelorder_naive(root1); cout<<endl;
+    // levelorder(root1); cout<<endl;
+    // levelorder_linebyline_1(root1); cout<<endl;
+    // levelorder_linebyline_2(root1); cout<<endl;
+
+    // cout<<"size :"<<getsize(root1)<<"\n";
+    // cout<<"max : "<<getmax(root1); cout<<endl;
+
+    // print_left_view_recursive(root1);
+    // print_left_view_iterative(root1);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -33,6 +52,8 @@ int main()
     root2->r->r = new node(1);
     root2->l->l->l = new node(1);
 
+    // if(isCsum(root2)) cout<<"YES\n";
+    // else cout<<"NO\n";
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -48,6 +69,19 @@ int main()
     root3->r->r = new node(6);
     root3->l->r->l = new node(8);
     root3->l->r->l->l = new node(9);//node added for disbalance
-
     
+    // cout<<"Height of tree : "<<height(root3)<<"\n";
+
+    // cout<<"is balanced :"; // naive
+    // isbalanced_naive(root3)?cout<<"yes\n":cout<<"no\n";
+
+    // cout<<"is balanced :"; // efficient
+    // (isbalanced(root3)!=-1)?cout<<"yes\n":cout<<"no\n";
+
+    // cout<<"max width : "<<max_width(root3);
+
+    //// FLATTENING THE TREE
+    // Node head = BTtoDLL_in(root3); printlist(head);
+
+
 }
--- tree/tree.h
+++ tree/tree.h
@@ -3,290 +3,35 @@
 #include<queue>
 #include<limits.h>
 using namespace std;
+
+// Node definition
 struct node
 {
-    int key;
-    int depth;
-    node * left;
-    node * right;
+    int val;
+    node * l;
+    node * r;
     node(int x)
     {
-        key = x;
-        left = right = NULL;
+        val = x;
+        l = r = NULL;
     }
 };
 
 typedef node * Node;
 
+// dfs traversals - working on 1st tree(root1)
+void preorder(Node root) {
 
-// DFS Traversals
-bool dfstraversal = true;
-void inorder(Node root)
-{
-    if(dfstraversal)
-    {
-        cout<<"InOrder : ";
-        dfstraversal = false;
-    }
-    if(root != NULL)
-    {
-        inorder(root->left);
-        cout<<root->key<<" ";
-        inorder(root->right);
-    }
-}
-
-void postorder(Node root)
-{
-    if(dfstraversal)
-    {
-        cout<<"PostOrder : ";
-        dfstraversal = false;
-    }
-    if(root != NULL)
-    {
-        inorder(root->left);
-        inorder(root->right);
-        cout<<root->key<<" ";
-    }
-}
-
-void preorder(Node root)
-{
-
-    if(dfstraversal)
-    {
-        cout<<"PreOrder : ";
-        dfstraversal = false;
-    }
-    if(root != NULL)
-    {
-        cout<<root->key<<" ";
-        inorder(root->left);
-        postorder(root->right);
-    }
-}
-
-
-// Height of Binary Tree
-int height(Node root)
-{
-    if(root == NULL) return 0;
-    else return max(height(root->left), height(root->right))+1;
-}
-
-void node_at_distance_(int k, Node root)
-{
-    if(root != NULL)
-    {
-        if(k==0) cout<<root->key<<" ";
-        else
-        {
-            node_at_distance_(k-1, root->left);
-            node_at_distance_(k-1, root->right);
-        }
-    }
-}
-
-//Breadth First search - Level order traversal
-// time complexity - Theta(N)
-// auxilary space - Theta(W) | W -> width of binary tree | 1<=W<=N
-void levelorder_naive(Node root)
-{
-    int h = height(root);
-    for(int i=1; i<=h; i++)
-    {
-        node_at_distance_(i-1, root);
-    }
-}
-
-void levelorder(Node root)
-{
     if(root == NULL) return;
-    queue<Node> q;
-    q.push(root);
-
-    while(q.empty() == false)
-    {
-        Node cur = q.front();
-        q.pop();
-        cout<<cur->key<<" ";
-        if(cur->left != NULL) q.push(cur->left);
-        if(cur->right != NULL) q.push(cur->right);
-    }
+    cout<<root->val<<" ";
+    preorder(root->l);
+    preorder(root->r);
 }
 
-void levelorder_linebyline_1(Node root)
-{
+void postorder(Node root) {
+
     if(root == NULL) return;
-    queue<Node> q;
-    q.push(root);
-    q.push(NULL);
-
-    while(q.size() > 1)
-    {
-        Node cur = q.front();
-        q.pop();
-        if(cur == NULL)
-        {
-            cout<<endl;
-            q.push(NULL);
-            continue;
-        }
-        cout<<cur->key<<" ";
-        if(cur->left != NULL) q.push(cur->left);
-        if(cur->right != NULL) q.push(cur->right);
-    }
-}
-
-void levelorder_linebyline_2(Node root) // T.C. O(N) | A.S. Theta(W-width of tree) or O(N)
-{
-    if(root == NULL) return;
-    queue<Node> q;
-    q.push(root);
-    while(q.empty()==false)
-    {
-        int count = q.size();
-        for(int i=0; i<count; i++)
-        {
-            Node cur = q.front();
-            q.pop();
-            cout<<cur->key<<" ";
-            if(cur->left != NULL) q.push(cur->left);
-            if(cur->right != NULL) q.push(cur->right);
-        }
-        cout<<endl;
-    }
-}
-
-// Size of a given binary TREE
-int getsize(Node root) // TC : O(N) | AS : O(H)
-{
-    if(root == NULL) return 0;
-    else return 1 + getsize(root->left) + getsize(root->right);
-}
-
-// Maximum in binary tree
-int getmax(Node root)
-{
-    if(root == NULL) return INT_MIN;
-    else return max(root->key, max(getmax(root->left), getmax(root->right)));
-}
-
-// Left view of Binary Tree
-int maxlevel = 0;
-void left_view_recursive(Node root, int level)
-{
-    if(root == NULL) return;
-    if(maxlevel <level)
-    {
-        cout<<root->key<<" ";
-        maxlevel = level;
-    }
-    left_view_recursive(root->left, level+1);
-    left_view_recursive(root->right, level+1);
-}
-
-void print_left_view_recursive(Node root)
-{
-    left_view_recursive(root, 1);
-}
-
-void print_left_view_iterative(Node root){
-    if(root==NULL) return;
-    queue<Node> q;
-    q.push(root);
-    while(q.empty()==false)
-    {
-        int count=q.size();
-        for(int i=0;i<count;i++)
-        {
-            Node curr=q.front();
-            q.pop();
-            if(i==0) cout<<curr->key<<" ";
-            if(curr->left!=NULL) q.push(curr->left);
-            if(curr->right!=NULL) q.push(curr->right);
-        }
-    }
-}
-
-//children sum property
-bool isCsum(Node root)
-{
-    if(root == NULL) return true;
-    if(root->left == NULL && root->right==NULL) return true;
-    int sum = 0;
-    if(root->left != NULL) sum += root->left->key;
-    if(root->right != NULL) sum += root->right->key;
-    return (root->key == sum && isCsum(root->left) && isCsum(root->right));
-}
-
-// Check if tree is Height balanced or not
-// binary tree in which the height of the left and right subtree of any node differ by not more than 1.
-bool isbalanced_naive(Node root) // O(N^2)
-{
-    if(root==NULL) return true;
-    int lh = height(root->left);
-    int rh = height(root->right);
-    return (abs(lh-rh)<=1 && isbalanced_naive(root->left) && isbalanced_naive(root->right));
-}
-
-int isbalanced(Node root) // O(N)
-{
-    if(root==NULL) return 0;
-    int lh = isbalanced(root->left);
-    if(lh==-1) return -1;
-    int rh = isbalanced(root->right);
-    if(rh==-1) return -1;
-    if(abs(lh-rh)>1) return -1;
-    else return (max(lh, rh) + 1);
-}
-
-int max_width(Node root) // A.S. Theta(width) | T.C. Theta(N)
-{
-    if(root == NULL) return 0;
-    queue<Node> q;
-    q.push(root);
-
-    int res = 0;
-
-    while(q.empty() == false)
-    {
-        int count = q.size();
-        res = max(res, count);
-        for(int i=0; i<count; i++)
-        {
-            Node curr = q.front();
-            q.pop();
-            if(curr->left != NULL) q.push(curr->left);
-            if(curr->right != NULL) q.push(curr->right);
-        }
-    }
-    return res;
-}
-
-// Flatten The Tree section
-Node BTtoDLL_in(Node root)
-{
-    static Node prev = NULL;
-    static Node head = NULL;
-    if(root == NULL) return root;
-    BTtoDLL_in(root->left);
-    if(prev == NULL) head = root;
-    else
-    {
-        root->left = prev;
-        prev->right = root;
-    }
-    prev = root;
-    BTtoDLL_in(root->right);
-    return head;
-}
-
-void printlist(Node head)
-{
-    cout<<"LIST : "; while(head->left != NULL) {
-        cout<<head->key<<" ";
-        head = head->left;
-    } cout<<endl;
+    postorder(root->l);
+    postorder(root->r);
+    cout<<root->val<<" ";
 }
