08ab0d20d5fbb8b9c0cda199ddece50277a78c9f
singhxayush <fanduayush2000@gmail.com>
2023-01-25 00:43:11 +0530

    x

--- seg tree/1.cpp
+++ seg tree/1.cpp
@@ -1,5 +1,10 @@
 #include "segtree.h"
 int main()
 {
-    
+    vector<int> a{1, 5, -1, 2, 1, 4, -3, 6, 2, 5};
+    Node root = build_st(a, 0, a.size()-1);
+
+    // levelorder(root);
+
+    int sum = querySt(root, 1, 9); cout<<sum<<"\n";
 }
