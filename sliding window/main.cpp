d9418113e1285b9d997c37d74eab7e6beab8256a
singhxayush <fanduayush2000@gmail.com>
2023-05-08 21:56:38 +0530

    slding windows first commit

---
+++ sliding window/main.cpp
@@ -0,0 +1,5 @@
+#include<slidingwindow.h>
+int main()
+{
+    
+}
---
+++ sliding window/slidingwindow.h
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(x) (sizeof(x)/sizeof(x[0]))
+#define pr(x) cout<<x<<endl;
+#define pr2(x, y) cout<<x<<' '<<y<<endl;
+#define pr3(x, y, z) cout<<x<<' '<<y<<' '<<z<<endl;
+
+/*
+    Slding Window
+    1. fixed size window
+    2. variable size window
+    
+*/
