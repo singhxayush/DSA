15cfad239937532a9dbcbd5bd31aed12ff40a4c2
singhxayush <fanduayush2000@gmail.com>
2023-06-08 13:07:12 +0530

    x

---
+++ 10.Stack&Queue/main.cpp
@@ -0,0 +1,18 @@
+#include"stack.h"
+
+int main()
+{
+    vector<int> v = {1, 4, 2, 0, 30, 1, 4, 6, 3, 0, 12};
+    // next_largest_elm(v);
+    // prev_largest_elm(v);
+    // next_smallest_elm(v);
+    prev_smallest_elm(v);
+    // stock_span(v);
+
+    // vector<int> v1{6, 2, 5, 4, 5, 1, 6};
+    // max_area_histogram(v1);
+    // max_area_histogram2(v1);
+
+    // vector<int> v2{3, 0, 0, 2, 0, 4};
+    // rain_water_trapping(v2);    
+}
