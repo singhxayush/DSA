c38d0faaa5ef7c175fb13d3380fa7d853d9dc206
singhxayush <fanduayush2000@gmail.com>
2024-02-29 02:18:43 +0530

    x

--- 06.Sorting/6a.cpp
+++ 06.Sorting/6a.cpp
@@ -3,10 +3,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int partition(int a[], int l, int h){
-
+int partition(int a[], int l, int h)
+{
     int p = a[h]; // PIVOT - here in this approach we assume that our pivot is always the last element!
     int i = l-1, j = l;
+    
     for(j = l; j <= h-1; j++)
     {
         if(a[j]<p)
@@ -22,15 +23,15 @@ int partition(int a[], int l, int h){
     return (i+1);
 }
 
-int main(){
-
+int main()
+{
     // int a[] = {5, 3, 12, 8, 5, 2};
     // int a[] = {10, 80, 30, 90, 40, 50, 70};
     // int a[] = {7, 6, 8, 4, 3};
     int a[] = {8, 4, 7, 9, 3, 10, 5};
 
     int n = sizeof(a)/sizeof(a[0]);
-    // int p; cin>>p;
+        // int p; cin>>p;
     int l = 0, h = n-1;
     partition(a, l, h);
 
